84beb09db34ff63e7238cb3ddeff029062047828
98thanhnhu98 <98thanhnhu98@gmail.com>
2023-03-07 16:38:16 +0700

    commit

--- index.java
+++ index.java
@@ -5,6 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
@@ -21,13 +25,14 @@ public class index {
         System.out.println("3. Delete a product");
         System.out.println("4. Search product");
         System.out.println("5. Update a product");
+        System.out.println("Chose : ");
     }
 
     public static void listProduct() {
         try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
             String strCurrentLine;
             while ((strCurrentLine = br.readLine()) != null) {
-                System.out.print(strCurrentLine.length());
+                System.out.print(strCurrentLine);
             }
             br.close();
         } catch (IOException e) {
@@ -59,6 +64,31 @@ public class index {
             System.out.println(e.getMessage());
         }
     }
+
+    public static void removeLineFromFile(String filePath, String lineToRemove) throws IOException {
+        Path path = Paths.get(filePath);
+        List<String> lines = Files.readAllLines(path);
+
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).equals(lineToRemove)) {
+                lines.remove(i);
+                break;
+            }
+        }
+
+        Files.write(path, lines);
+    }
+
+    public static void updateProductFromFile(int idc){
+        try {
+            String id = String.valueOf(idc);
+            String lineToRemove = search(id);
+            removeLineFromFile(FILENAME1, lineToRemove);
+            System.out.println("Line removed successfully.");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
     public static void updateProductWithUtils(int idc) {
         try {
             LineIterator iterator = FileUtils.lineIterator(new File(FILENAME), "UTF-8");
@@ -66,12 +96,27 @@ public class index {
                 String line = iterator.nextLine();
                 String[] parts = line.substring(2, line.length() - 2).split(" \\| ");
                 int id = Integer.parseInt(parts[0].split(" = ")[1]);
-                String name = parts[1].split(" = ")[1];
-                String nhaSanXuat = parts[2].split(" = ")[1];
-                String dongSanPham = parts[3].split(" = ")[1];
-                String giaSanPhamStr = parts[4].split(" = ")[1];
                 if (id == idc) {
-                    String newLine = id + "," + name + "," + nhaSanXuat + "," + dongSanPham + ","+ giaSanPhamStr;
+//                    line
+                    System.out.println("Enter a product name : ");
+                    String name = sc.next();
+                    System.out.println("Enter the product manufacturer name :");
+                    String nhaSanXuat = sc.next();
+                    System.out.println("Please select the product line\n1. Iphone \2. Android");
+                    String dongSanPham = "";
+                    int chose = sc.nextInt();
+                    if (chose == 1) {
+                        dongSanPham = "Iphone";
+                    } else if (chose == 2) {
+                        dongSanPham = "Android";
+                    }
+                    System.out.println("Enter a price for the product : ");
+                    long giaSanPham = sc.nextLong();
+                    if(giaSanPham < 10000){
+                        throw new Exception("price must not be less than 10000");
+                    }
+                    DienThoai dt = new DienThoai(id, name, dongSanPham, dongSanPham, giaSanPham);
+                    String newLine = dt.toString();
                     FileUtils.write(new File(FILENAME), newLine + System.lineSeparator(), "UTF-8", true);
                     System.out.println("Dữ liệu đã được cập nhật thành công.");
                 } else {
@@ -79,6 +124,8 @@ public class index {
                 }
             }
         }catch(IOException e){
+            e.printStackTrace();
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
@@ -146,7 +193,7 @@ public class index {
     public static void addProduct() {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILENAME, true))) {
             int id = 0;
-            if (getMax() == 0) {
+            if (getMax() == -1) {
                 throw new Exception("ERROR WHEN RETRIEVING THE LARGEST ID");
             } else {
                 id = getMax() + 1;
@@ -187,7 +234,10 @@ public class index {
             } while (chose != 1 || chose != 2);
             System.out.println("Enter a price for the product : ");
             long giaSanPham = sc.nextLong();
-            DienThoai dt = new DienThoai(id, dongSanPham, dongSanPham, dongSanPham, giaSanPham);
+            if(giaSanPham < 10000){
+                throw new Exception("price must not be less than 10000");
+            }
+            DienThoai dt = new DienThoai(id, tenSanPham, nhaSanPham, dongSanPham, giaSanPham);
             bw.newLine();
             bw.append(dt.toString());
             bw.flush();
@@ -196,34 +246,60 @@ public class index {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            System.out.println(
-                    "---------------\n The program has been error , the product wont add to List \n--------------------\n");
         }
     }
 
     public static void deleteWithUtils(int idc) {
         try {
-            LineIterator iterator = FileUtils.lineIterator(new File(FILENAME), "UTF-8");
-            while (iterator.hasNext()) {
-                String line = iterator.nextLine();
-                String[] parts = line.substring(2, line.length() - 2).split(" \\| ");
-                int id = Integer.parseInt(parts[0].split(" = ")[1]);
-                String name = parts[1].split(" = ")[1];
-                String nhaSanXuat = parts[2].split(" = ")[1];
-                String dongSanPham = parts[3].split(" = ")[1];
-                String giaSanPhamStr = parts[4].split(" = ")[1];
-                if (id == idc) {
-                    String newLine = id + "," + name + "," + nhaSanXuat + "," + dongSanPham + "," + giaSanPhamStr;
-                    System.out.println(newLine);
+            String id = String.valueOf(idc);
+            String lineToRemove = search(id);
+            Path path = Paths.get(FILENAME1);
+            List<String> lines = Files.readAllLines(path);
+            for (int i = 0; i < lines.size(); i++) {
+                if (lines.get(i).equals(lineToRemove)) {
+                    lines.remove(i);
+                    break;
                 }
             }
+            Files.write(path, lines);
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(search("3"));
+        boolean flag = true;
+        int a = 0;
+        while(flag){
+            menu();
+            a = sc.nextInt();
+            switch (a){
+                case 1:
+                    addProduct();
+                    break;
+                case 2:
+                    listProductWithUtils();
+                    break;
+                case 3:
+                    System.out.println("Input id");
+                    int idc = sc.nextInt();
+                    updateProductWithUtils(idc);
+                    break;
+                case 4:
+                    System.out.println("Input name or id");
+                    String noi = sc.next();
+                    System.out.println(search(noi));
+                    break;
+                case 5:
+                    System.out.println("Input id");
+                    int id = sc.nextInt();
+                    deleteWithUtils(id);
+                    break;
+                default:
+                    System.out.println("Out.....");
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
